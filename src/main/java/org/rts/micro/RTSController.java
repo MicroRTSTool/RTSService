e4fb223a0fbc02e29ce26812b3a1ecb9cf86df75
Fathima Dilhasha <dilhasha.uom@gmail.com>
2023-05-27 22:57:17 +0530

    Add cors details to API

--- src/main/java/org/rts/micro/RTSController.java
+++ src/main/java/org/rts/micro/RTSController.java
@@ -1,6 +1,7 @@
 package org.rts.micro;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Set;
 @RestController
 public class RTSController {
 
+    @CrossOrigin
     @GetMapping("/configure")
     public ResponseEntity<String> configureRepo(@RequestParam String repoName, @RequestParam String branchName,
                                                 @RequestParam String monitoringURL) {
@@ -19,9 +21,11 @@ public class RTSController {
             return ResponseEntity.badRequest().body("Error: " + e.getMessage());
         }
         String message = "Successfully configured Repo: " + repoName + ", Branch: " + branchName;
+        System.out.println(message);
         return ResponseEntity.ok(message);
     }
 
+    @CrossOrigin
     @GetMapping("/rts")
     public ResponseEntity<String> selectTests(@RequestParam int pr, @RequestParam String repoName,
                                               @RequestParam String branchName) {
